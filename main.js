faeeabfe87c4340ee695934fa0c02667c74feba5
Dletta <jcduschletta@gmail.com>
2020-01-23 17:26:04 -0800

    url3

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var Gitter = require('node-gitter');
 var Discord = require('discord.js');
 var Gun = require('gun');
 
-var peers = ["http://localhost:8765"]
+var peers = ["http://localhost:8765/gun"]
 var gun = Gun({peers:peers, radisk:false, localStorage:false})
 
 gun.get('grappling-gun').get('version').once((node, key)=>console.log(key, node));
